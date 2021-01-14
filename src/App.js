ebd878114bce9e48b5f12a541644a034f21b6974
hyeon ji <inthefield1027@gmail.com>
2021-01-14 22:19:27 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
-import React, { Component } from "react";
-import "./App.css";
-import axios from "axios";
-import Movie from "./Movie";
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-class App extends Component {
-    state = {
-        isLoading: true,
-        movies: [],
-    };
-    getMovies = async () => {
-        const {
-            data: {
-                data: { movies },
-            },
-        } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-        this.setState({ movies, isLoading: false });
-        console.log(movies);
-    };
-    //render호출되고 호출됨
-    componentDidMount() {
-        this.getMovies();
-    }
-    render() {
-        const { isLoading, movies } = this.state;
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                        <span className="loader__text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="movies">
-                        {movies.map((movie) => (
-                            <Movie
-                                key={movie.id}
-                                id={movie.id}
-                                title={movie.title}
-                                year={movie.year}
-                                summary={movie.summary}
-                                poster={movie.medium_cover_image}
-                                genres={movie.genres}
-                            />
-                        ))}
-                    </div>
-                )}
-            </section>
-        );
-    }
+function App() {
+    return (
+        <HashRouter>
+            <Navigation />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+        </HashRouter>
+    );
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./Home.css";
+import axios from "axios";
+import Movie from "../components/Movie";
+
+class Home extends Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+        this.setState({ movies, isLoading: false });
+        console.log(movies);
+    };
+    //render호출되고 호출됨
+    componentDidMount() {
+        this.getMovies();
+    }
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader__text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map((movie) => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                title={movie.title}
+                                year={movie.year}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
+        );
+    }
+}
+
+export default Home;
